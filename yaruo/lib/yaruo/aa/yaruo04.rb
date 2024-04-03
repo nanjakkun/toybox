06a3915381aef4e0b2bb061911ef5b70ba2625f3
KIMURA Takahiro <nanjakkun@gmail.com>
2024-04-04 06:08:42 +0900

    yaruo04

---
+++ yaruo/lib/yaruo/aa/yaruo04.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# ｷﾞｭｯ
+module Yaruo
+  module Aa
+    class Yaruo04 < ::Yaruo::Aa::Base
+      def self.aa
+        """
+
+　　　　　　　　　 ／￣￣＼
+　　　　　　　　.ノ　 ＼,_.　　.＼
+　　　　　　　（＞）（＜ ） 　　　|
+　 　　　／￣（__人__） 　　　　|
+　　　／　 _ノ .ヾ⌒ ´　　 　　|
+　 ／ 　oﾟ⌒　　{ 　 　　 .　　/
+　 |　　　　 （__人＼　　　　.ｶ
+　 ＼　　 　 ｀ ⌒´ﾝ 　 　 .ﾉﾉ　　ｷﾞｭｯ
+　　 ／ ￣　 　.／　　. ..　し｝
+　　　　　.(⌒二_⊿ニ⌒) 　.i
+
+        """
+      end
+    end
+  end
+end
