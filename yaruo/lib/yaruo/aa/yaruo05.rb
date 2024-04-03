938b006c808c6f019010d7fcd9014efa73552ded
KIMURA Takahiro <nanjakkun@gmail.com>
2024-04-04 06:10:26 +0900

    yaruo05

---
+++ yaruo/lib/yaruo/aa/yaruo05.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# こいつ最高にアホ
+module Yaruo
+  module Aa
+    class Yaruo05 < ::Yaruo::Aa::Base
+      def self.aa
+        """
+　　　　　　　　　　　　 　 ∩＿
+　　　　　　　　　　　　　　〈〈〈　ヽ
+　　　　　　＿＿＿_　　　〈⊃　　}
+　　　　 ／⌒　　⌒＼　　 |　　 |
+　　　／（ ●） 　（●）＼　 !　　 !
+　 ／ :::::⌒（__人__）⌒:::::＼|　　 l
+　 |　　　　　|r┬-| 　 　 　 | 　／　＜こいつ最高にアホ
+　 ＼ 　 　　｀ ー'´ 　 　 ／／
+　　/　＿＿　 　　　　　　/
+　　(＿＿＿）　　　　　　/
+        """
+      end
+    end
+  end
+end
