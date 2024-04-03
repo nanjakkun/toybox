7f2851f58f5ddd13e263e6b2b463b1e5e39066b4
KIMURA Takahiro <nanjakkun@gmail.com>
2024-04-04 06:12:31 +0900

    giko01

---
+++ yaruo/lib/yaruo/aa/giko01.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# トボトボ
+module Yaruo
+  module Aa
+    class Giko01 < ::Yaruo::Aa::Base
+      def self.aa
+        """
+　　　∧∧
+　　 /⌒ヽ)
+　　i三　∪
+　○三　|
+　　(/~∪
+　　三三
+　三三
+三三三
+        """
+      end
+    end
+  end
+end
