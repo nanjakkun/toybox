faf06d70e7f6f9b4c53f114d35878e89dbbaf178
KIMURA Takahiro <nanjakkun@gmail.com>
2024-04-05 05:42:47 +0900

    Add good AA

---
+++ yaruo/lib/yaruo/aa/iine01.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# イイね
+module Yaruo
+  module Aa
+    class Iine01 < ::Yaruo::Aa::Base
+      def self.aa
+        """
+/j^i
+　　　　　 ./　 ;!
+　　　　　/　 /_＿,,..
+　　　　/　　`(_t＿,__〕
+　　　 /　　　 '(_t＿,__〕
+　　　/　　　　｛_i＿,__〕
+　 ／　　　 ノ　 {_i＿_〉
+／　　　　　　＿,..-'\"
+        """
+      end
+    end
+  end
+end
