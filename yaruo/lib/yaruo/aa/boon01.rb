2416ba3e1938476bfba0c4b224e0282a5481f6c5
KIMURA Takahiro <nanjakkun@gmail.com>
2024-05-04 02:32:47 +0900

    Add boon

---
+++ yaruo/lib/yaruo/aa/boon01.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# ブーン
+module Yaruo
+  module Aa
+    class Boon01 < ::Yaruo::Aa::Base
+      def self.aa
+        """
+　　　　 　　／⌒ヽ
+⊂二二二（　＾ω＾）二⊃
+　　　　　　|　　　 / 　　　　　　ﾌﾞｰﾝ
+　　 　　　 （　ヽノ
+　　　　　　 ﾉ>ノ
+　　 三　　レﾚ
+
+        """
+      end
+    end
+  end
+end
